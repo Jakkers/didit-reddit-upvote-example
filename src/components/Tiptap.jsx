1dbda406b0770d01c5cb96bbb15e07c0a54dcf26
Jake Darby <designdarby@gmail.com>
2024-07-31 14:53:35 +0100

    configure custom comment

--- src/components/Tiptap.jsx
+++ src/components/Tiptap.jsx
@@ -2,15 +2,28 @@
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import customComment from "@rcode-link/tiptap-comments";
 
 // import { CommentFormButton } from "./CommentFormButton";
 
 const Tiptap = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      customComment.configure({
+        user: {
+          user_id: session.user.id,
+          post_id: postId,
+          parent_comment_id: parentCommentId,
+          name: "comment",
+        },
+      }),
+    ],
+
+    //session.user.id, postId, parentCommentId, formData.get("comment")
+
     content: "<p>Type your comment...</p>",
     immediatelyRender: false,
-    name: "comment",
   });
 
   return (
